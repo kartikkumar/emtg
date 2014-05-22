9f2316f410882619a7eaad9263e130f456a933a3
nasachilla <nasachilla@77bb456d-b9d8-4424-8c4a-7c450a671a84>
2014-05-22 18:07:47 +0000

    The segmentation fault handler should NOT be anywhere in the code but in EMTGv8.cpp!
    git-svn-id: svn://svn.code.sf.net/p/emtg/emtg-goddard@101 77bb456d-b9d8-4424-8c4a-7c450a671a84

--- branch/EMTG-unitscrubbing/src/lazy_race_tree_search.h
+++ branch/EMTG-unitscrubbing/src/lazy_race_tree_search.h
@@ -31,23 +31,6 @@ using namespace boost::filesystem;
 using namespace boost::gregorian;
 using namespace boost::posix_time;
 
-#ifdef _STONEAGECplusplus
-#include <execinfo.h>
-#include <signal.h>
-void handler(int sig) {
-	void *array[10];
-	size_t size;
-
-	// get void*'s for all entries on the stack
-	size = backtrace(array, 10);
-
-	// print out all the frames to stderr
-	fprintf(stderr, "Error: signal %d:\n", sig);
-	backtrace_symbols_fd(array, size, STDERR_FILENO);
-	exit(1);
-}
-#endif
-
 namespace EMTG{
 
 	void lazy_race_tree_search(missionoptions * options, boost::ptr_vector<Astrodynamics::universe> & TheUniverse_in);
