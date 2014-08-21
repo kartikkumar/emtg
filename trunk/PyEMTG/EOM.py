1. Fixed bug in FBLT output. Mass flow rate was coming out in mass units per time unit, not kg/s. This led to bad data being fed into PyEMTG's numerical integrator, causing the graphical glitch when plotting FBLT trajectories. I've been hunting this one for almost a year now!
2. zero-turn flybys are now output as "zeroflyby" instead of as "departure".

3. getting started on having all phase types recognize when they are not the first journey but start at a free point in space. I want eventually want them to grab the previous journey's endpoint instead of starting over. That way we can have multiple non-body orbit transfers in one mission.

git-svn-id: svn://svn.code.sf.net/p/emtg/emtg-goddard@287 77bb456d-b9d8-4424-8c4a-7c450a671a84

trunk/PyEMTG/EOM.py
@@ -4,7 +4,6 @@ def EOM_inertial_2bodyconstant_thrust(t, X, Thrust, Mdot, mu):
     r = np.linalg.norm(X[0:3])
     r3 = r**3
     dX = np.zeros(7)
-    g0 = 9.80665
 
     dX[0] = X[3]
     dX[1] = X[4]
@@ -15,6 +14,25 @@ def EOM_inertial_2bodyconstant_thrust(t, X, Thrust, Mdot, mu):
     dX[6] = -Mdot
 
     return dX
+
+def EOM_jacobian_intertial_2bodyconstant_thrust(t, X, Thrust, Mdot, mu):
+    r = np.linalg.norm(X[0:3])
+
+    r3 = r**3
+    r5 = r3*r*r
+    ddX = np.zeros((7,7))
+
+    ddX[0,3] = 1.0
+    ddX[1,4] = 1.0
+    ddX[2,5] = 1.0
+    ddX[3,0] = -mu * (1/r3 - 3 * X[0] * X[0] / r5)
+    ddX[3,6] = -Thrust[0] / (X[6]**2)
+    ddX[4,1] = -mu * (1/r3 - 3 * X[1] * X[1] / r5)
+    ddX[4,6] = -Thrust[1] / (X[6]**2)
+    ddX[5,2] = -mu * (1/r3 - 3 * X[2] * X[2] / r5)
+    ddX[5,6] = -Thrust[2] / (X[6]**2)
+
+    return ddX
 
 def EOM_inertial_2body(t, X, mu):
     r = np.linalg.norm(X[0:3])
@@ -29,3 +47,19 @@ def EOM_inertial_2body(t, X, mu):
     dX[5] = -mu*X[2]/r3
 
     return dX
+
+def EOM_jacobian_intertial_2body(t, X, mu):
+    r = np.linalg.norm(X[0:3])
+
+    r3 = r**3
+    r5 = r3*r*r
+    ddX = np.zeros((6,6))
+
+    ddX[0,3] = 1.0
+    ddX[1,4] = 1.0
+    ddX[2,5] = 1.0
+    ddX[3,0] = -mu * (1/r3 - 3 * X[0] * X[0] / r5)
+    ddX[4,1] = -mu * (1/r3 - 3 * X[1] * X[1] / r5)
+    ddX[5,2] = -mu * (1/r3 - 3 * X[2] * X[2] / r5)
+
+    return ddX

trunk/PyEMTG/MissionEvent.py
@@ -121,21 +121,21 @@ class MissionEvent(object):
                 CenterPointStateAfterManeuver = copy.deepcopy(CenterPointState)
                 CenterPointStateAfterManeuver[3:6] += np.array(self.DeltaVorThrustVectorControl) * TU /LU
 
-                ForwardIntegrateObject = ode(EOM.EOM_inertial_2body).set_integrator('dop853', atol=1.0e-6, rtol=1.0e-6)
-                ForwardIntegrateObject.set_initial_value(CenterPointStateAfterManeuver).set_f_params(1.0)
+                ForwardIntegrateObject = ode(EOM.EOM_inertial_2body, jac=EOM.EOM_jacobian_intertial_2body).set_integrator('dop853', atol=1.0e-6, rtol=1.0e-6)
+                ForwardIntegrateObject.set_initial_value(CenterPointStateAfterManeuver).set_f_params(1.0).set_jac_params(1.0)
 
-                dt = self.TimestepLength * 86400 / TU / 10
+                dt = self.TimestepLength * 86400.0 / TU / 10
                 StateHistoryForward = []
-                while ForwardIntegrateObject.successful() and ForwardIntegrateObject.t < self.TimestepLength * 86400 / TU / 2.0:
+                while ForwardIntegrateObject.successful() and ForwardIntegrateObject.t < self.TimestepLength * 86400.0 / TU / 2.0:
                     ForwardIntegrateObject.integrate(ForwardIntegrateObject.t + dt)
                     StateHistoryForward.append(ForwardIntegrateObject.y * LU)
 
-                BackwardIntegrateObject = ode(EOM.EOM_inertial_2body).set_integrator('dop853', atol=1.0e-6, rtol=1.0e-6)
-                BackwardIntegrateObject.set_initial_value(CenterPointState).set_f_params(1.0)
+                BackwardIntegrateObject = ode(EOM.EOM_inertial_2body, jac=EOM.EOM_jacobian_intertial_2body).set_integrator('dop853', atol=1.0e-6, rtol=1.0e-6)
+                BackwardIntegrateObject.set_initial_value(CenterPointState).set_f_params(1.0).set_jac_params(1.0)
 
-                dt = self.TimestepLength * 86400 / TU / 10
+                dt = self.TimestepLength * 86400.0 / TU / 10
                 StateHistoryBackward = []
-                while BackwardIntegrateObject.successful() and BackwardIntegrateObject.t > -self.TimestepLength * 86400 / TU / 2.0:
+                while BackwardIntegrateObject.successful() and BackwardIntegrateObject.t > -self.TimestepLength * 86400.0 / TU / 2.0:
                     BackwardIntegrateObject.integrate(BackwardIntegrateObject.t - dt)
                     StateHistoryBackward.append(BackwardIntegrateObject.y * LU)
 
@@ -168,24 +168,24 @@ class MissionEvent(object):
                 CenterPointState[0:6] = np.array(self.SpacecraftState) / LU
                 CenterPointState[3:6] *= TU
                 CenterPointState[6] = 1.0
-                ScaledThrust = np.array(self.Thrust) / self.Mass / LU / 1000* TU*TU
+                ScaledThrust = np.array(self.Thrust) * TU * TU / 1000.0 / self.Mass / LU
                 ScaledMdot = self.MassFlowRate / self.Mass * TU
 
-                ForwardIntegrateObject = ode(EOM.EOM_inertial_2bodyconstant_thrust).set_integrator('dop853', atol=1.0e-6, rtol=1.0e-6)
-                ForwardIntegrateObject.set_initial_value(CenterPointState).set_f_params(ScaledThrust, ScaledMdot, 1.0)
+                ForwardIntegrateObject = ode(EOM.EOM_inertial_2bodyconstant_thrust, jac=EOM.EOM_jacobian_intertial_2bodyconstant_thrust).set_integrator('dop853', atol=1.0e-6, rtol=1.0e-6)
+                ForwardIntegrateObject.set_initial_value(CenterPointState).set_f_params(ScaledThrust, ScaledMdot, 1.0).set_jac_params(ScaledThrust, ScaledMdot, 1.0)
 
-                dt = self.TimestepLength * 86400 / TU / 10
+                dt = self.TimestepLength * 86400.0 / TU / 10
                 StateHistoryForward = []
-                while ForwardIntegrateObject.successful() and ForwardIntegrateObject.t < self.TimestepLength * 86400 / TU / 2.0:
+                while ForwardIntegrateObject.successful() and ForwardIntegrateObject.t < self.TimestepLength * 86400.0 / TU / 2.0:
                     ForwardIntegrateObject.integrate(ForwardIntegrateObject.t + dt)
                     StateHistoryForward.append(ForwardIntegrateObject.y * LU)
 
-                BackwardIntegrateObject = ode(EOM.EOM_inertial_2bodyconstant_thrust).set_integrator('dop853', atol=1.0e-6, rtol=1.0e-6)
-                BackwardIntegrateObject.set_initial_value(CenterPointState).set_f_params(ScaledThrust, ScaledMdot, 1.0)
+                BackwardIntegrateObject = ode(EOM.EOM_inertial_2bodyconstant_thrust, jac=EOM.EOM_jacobian_intertial_2bodyconstant_thrust).set_integrator('dop853', atol=1.0e-6, rtol=1.0e-6)
+                BackwardIntegrateObject.set_initial_value(CenterPointState).set_f_params(ScaledThrust, ScaledMdot, 1.0).set_jac_params(ScaledThrust, ScaledMdot, 1.0)
 
-                dt = self.TimestepLength * 86400 / TU / 10
+                dt = self.TimestepLength * 86400.0 / TU / 10
                 StateHistoryBackward = []
-                while BackwardIntegrateObject.successful() and BackwardIntegrateObject.t > -self.TimestepLength * 86400 / TU / 2.0:
+                while BackwardIntegrateObject.successful() and BackwardIntegrateObject.t > -self.TimestepLength * 86400.0 / TU / 2.0:
                     BackwardIntegrateObject.integrate(BackwardIntegrateObject.t - dt)
                     StateHistoryBackward.append(BackwardIntegrateObject.y * LU)
 
@@ -218,7 +218,7 @@ class MissionEvent(object):
         elif self.EventType == "match_point":
             GraphicsObject.scatter(self.SpacecraftState[0], self.SpacecraftState[1], self.SpacecraftState[2], s=4, c='k', marker='s')
 
-        if PlotOptions.ShowTextDescriptions and not (self.EventType == 'coast' or self.EventType == 'force-coast' or self.EventType == "SFthrust" or self.EventType == "FBLTthrust" or self.EventType == "FBLTSthrust" or self.EventType == "match_point"):
+        if PlotOptions.ShowTextDescriptions and not (self.EventType == 'zeroflyby' or self.EventType == 'coast' or self.EventType == 'force-coast' or self.EventType == "SFthrust" or self.EventType == "FBLTthrust" or self.EventType == "FBLTSthrust" or self.EventType == "match_point"):
             self.LabelEvent(GraphicsObject, PlotOptions)
 
     def LabelEvent(self, GraphicsObject, PlotOptions):
@@ -283,7 +283,7 @@ class MissionEvent(object):
             self.rcid = GraphicsObject.figure.canvas.mpl_connect('button_release_event', self.ReleaseAnnotation)
 
     def UpdateLabelPosition(self, Figure, Axes):
-        if not (self.EventType == 'coast' or self.EventType == 'force-coast' or self.EventType == "SFthrust" or self.EventType == "FBLTthrust" or self.EventType == "FBLTSthrust" or self.EventType == "match_point" or (self.EventType == "chem_burn" and self.DVmagorThrottle < 0.001)):
+        if not (self.EventType == 'zeroflyby' or self.EventType == 'coast' or self.EventType == 'force-coast' or self.EventType == "SFthrust" or self.EventType == "FBLTthrust" or self.EventType == "FBLTSthrust" or self.EventType == "match_point" or (self.EventType == "chem_burn" and self.DVmagorThrottle < 0.001)):
             x2, y2, _ = proj3d.proj_transform(self.SpacecraftState[0],self.SpacecraftState[1],self.SpacecraftState[2], Axes.get_proj())
             self.eventlabel.xy = x2,y2
             self.eventlabel.update_positions(Figure.canvas.renderer)